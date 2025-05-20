abf7cd6e0c61cd3cf6d11c472f70d1ed953f91c9
h.yamamoto.bs <h.yamamoto.bs@gmail.com>
2025-05-21 06:47:34 +0900

    setting.pyの修正

--- config/settings.py
+++ config/settings.py
@@ -142,7 +142,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = DASE_DIR / '/static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # 追記
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',  # 追記
